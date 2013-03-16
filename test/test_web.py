a1448502a978d72116ea79b40ea19690f3b8b4c9
FND <fnd@localhost.localdomain>
2013-03-16 12:46:48 +0100

    exposed tag listing via HTTP

--- test/test_web.py
+++ test/test_web.py
@@ -1,3 +1,5 @@
+import os
+
 import httplib2
 import wsgi_intercept
 
@@ -5,17 +7,41 @@ from UserDict import UserDict # XXX: is this what we want?
 
 from wsgi_intercept import httplib2_intercept
 
+from tiddlyweb.model.bag import Bag
 from tiddlyweb.config import config
 from tiddlyweb.web.serve import load_app
 
+from tiddlywebplugins.utils import get_store
+
 import tiddlywebplugins.tagdex as tagdex
+import tiddlywebplugins.tagdex.database as database
 
 
 def setup_module(module):
-    module.CONFIG = { 'tagdex_db': 'tagdex_test.sqlite' }
-    module.STORE = UserDict()
-    module.STORE.environ = { 'tiddlyweb.config': CONFIG }
-    _initialize_app(module.CONFIG)
+    cfg = _initialize_app({ 'tagdex_db': 'tagdex_test.sqlite' })
+    store = get_store(cfg)
+
+    # reset database
+    db = database._db_path(cfg)
+    try:
+        os.remove(db)
+    except OSError:
+        pass
+
+    bag = Bag('alpha')
+    store.put(bag)
+
+    _put_tiddler('HelloWorld', bag.name, """
+tags: foo bar
+
+lorem ipsum
+dolor sit amet
+    """.strip())
+    _put_tiddler('Lipsum', bag.name, """
+tags: bar baz
+
+...
+    """.strip())
 
 
 def test_web_collection():
@@ -25,6 +51,17 @@ def test_web_collection():
 
     assert response.status == 200
     assert response['content-type'] == 'text/plain'
+    assert content == 'foo\nbar\nbaz\n'
+
+
+def _put_tiddler(title, bag, body):
+    http = httplib2.Http()
+    uri = 'http://example.org:8001/bags/%s/tiddlers/%s' % (bag, title)
+    response, content = http.request(uri, method='PUT',
+            headers={ 'Content-Type': 'text/plain' }, body=body)
+
+    if not response.status == 204:
+        raise RuntimeError(content)
 
 
 def _initialize_app(cfg):
@@ -38,3 +75,5 @@ def _initialize_app(cfg):
 
     httplib2_intercept.install()
     wsgi_intercept.add_wsgi_intercept('example.org', 8001, lambda: load_app())
+
+    return config
--- tiddlywebplugins/tagdex/web.py
+++ tiddlywebplugins/tagdex/web.py
@@ -1,3 +1,13 @@
+from __future__ import absolute_import, with_statement
+
+from . import database
+
+
 def get_tags(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ["lorem ipsum"]
+
+    with database.Connection(environ['tiddlyweb.config']) as (conn, cur):
+        tags = [row[0] for row in cur.execute('SELECT name FROM tags')]
+        # TODO: stream generator from the database?
+
+    return ('%s\n' % tag for tag in tags) # TODO: paging
